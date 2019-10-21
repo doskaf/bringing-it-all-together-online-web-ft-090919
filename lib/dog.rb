ae25967dec909af14500fa052bdcf9ab49dc590c
Dominique Skaf <doskaf@yahoo.com>
2019-10-21 00:28:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,8 +43,13 @@ class Dog
     self.new(id: row[0], name: row[1], breed: row[2])
   end
   
-  def self.find_by_id
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+    SQL
     
+    DB[:conn].execute(sql, id)
   end
   
 end
