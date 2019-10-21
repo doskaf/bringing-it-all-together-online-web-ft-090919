5b5b28a29df83761089d2c7b844f22e88cf8e292
Dominique Skaf <doskaf@yahoo.com>
2019-10-21 00:39:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,8 +54,10 @@ class Dog
   end
   
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
-     info = DB[:conn].execute(sql, name, breed)[0]
+      sql = <<-SQL
+                SELECT * FROM dogs WHERE name = ? AND breed = ?;
+            SQL
+        info = DB[:conn].execute(sql, name, breed)[0]
         if !info.nil? && info.length > 0
             self.new_from_db(info)
         else    
